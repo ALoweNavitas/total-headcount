2a3122f8217df61c051b52ce36b080f4f87e96e8
ALoweNavitas <adam.lowe@navitas.com>
2022-01-17 23:56:28 +0000

    Updated code for security

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import os
 
 dir = os.getcwd()
 
-input = []
-output = []
+import_file = []
+output_file = []
 
 # Locate the export file
 for file in os.listdir(os.path.join(dir, 'export')):
     if file.endswith('.xlsx'):
         fp = os.path.basename(file)
-        input.append(os.path.join(dir, "export", fp).replace("\\", "/"))
+        import_file.append(os.path.join(dir, "export", fp).replace("\\", "/"))
 
 # Locate the output file
 for file in os.listdir(os.path.join(dir, 'output')):
     if file.endswith('.xlsx'):
         fp = os.path.basename(file)
-        output.append(os.path.join(dir, 'output', fp).replace("\\", "/"))
+        output_file.append(os.path.join(dir, 'output', fp).replace("\\", "/"))
 
 # Read in the export file
-df = pd.read_excel(input[0])
+df = pd.read_excel(import_file[0])
 
 # Filter the dataframe to only have UPE records
 upe_filter = df['Contract - Team'] == 'UPE'
@@ -38,7 +38,8 @@ df = df[[
 ]]
 
 # Export to Excel
-workbook = xw.Book(output[0], password='password')
+password = input('Enter the Excel file password: ')
+workbook = xw.Book(output_file[0], password='password')
 ws = workbook.sheets['head count']
 ws.range('A4').options(index=False, header=False).value = df
 
